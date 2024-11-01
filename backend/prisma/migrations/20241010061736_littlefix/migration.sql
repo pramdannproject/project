559983595b0c9a5d2476e10a6c131045ee0d90eb
pramdannproject <pramdanip@gmail.com>
2024-11-01 20:47:43 +0700

    update

--- backend/prisma/migrations/20241010061736_littlefix/migration.sql
+++
@@ -1,4 +0,0 @@
--- AlterTable
-ALTER TABLE "ControlMachining" ALTER COLUMN "camshaft_conv_bal_in" SET DEFAULT 0,
-ALTER COLUMN "camshaft_conv_bal_ex" SET DEFAULT 0,
-ALTER COLUMN "camshaft_hv_bal_in" SET DEFAULT 0;
